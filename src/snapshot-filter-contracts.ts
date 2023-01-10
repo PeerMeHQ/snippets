69d6b7c920b13c2a83a4a3b0b12cc7aa3d690517
micha vie <micha@freeiam.com>
2023-01-10 18:46:59 +0100

    add snapshot contract filter

---
+++ src/snapshot-filter-contracts.ts
@@ -0,0 +1,27 @@
+import fs from 'fs'
+import path from 'path'
+import collect from 'collect.js'
+import { getArg, loadJsonData, saveJsonData } from './shared/helpers'
+import { Address } from '@elrondnetwork/erdjs/out'
+
+// ts-node src/snapshot-filter-contracts.ts
+
+// arg 1: input file
+// arg 2: output file
+const main = async () => {
+  const inputFile = getArg(0)
+  const outputFile = getArg(1)
+
+  const contents = await (<Promise<string[]>>loadJsonData(inputFile))
+
+  const filtered = collect(contents)
+    .reject(address => Address.fromBech32(address).isContractAddress())
+    .all()
+
+  saveJsonData(outputFile, filtered)
+
+  console.log('🗑️', `Filtered ${contents.length - filtered.length} smart contract addresses.`)
+  console.log('✅', `Saved ${filtered.length} items to ${outputFile}!`)
+}
+
+main()
