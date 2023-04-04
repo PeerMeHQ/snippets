c02cf82b0361b9707e9f9e1f174d340910c0e702
micha vie <micha@freeiam.com>
2023-04-04 17:21:20 +0200

    add xbulk fake dist snippet for testing

---
+++ src/generate-fake-xbulk-distribution.ts
@@ -0,0 +1,31 @@
+import { getArg } from './shared/helpers'
+import { Mnemonic } from '@elrondnetwork/erdjs-walletcore/out'
+
+// ts-node src/generate-fake-xbulk-distribution.ts {amount} {minValue} {maxValue}
+
+const main = async () => {
+  const amount = +getArg(0)
+  const minValue = +getArg(1)
+  const maxValue = +getArg(2)
+
+  console.log(`generating fake distribution with ${amount} lines...`)
+
+  const generatePubKey = () =>
+    Mnemonic.generate()
+      .deriveKey(0)
+      .generatePublicKey()
+
+  const generateRandomValue = () => {
+    const randomValue = Math.random() * (maxValue - minValue) + minValue
+    return parseFloat(randomValue.toFixed(2))
+  }
+
+  const randomAddresses = Array.from({ length: amount })
+    .map(generatePubKey)
+    .map(key => key.toAddress().bech32() + ',' + generateRandomValue().toString())
+    .join('\n')
+
+  console.log(randomAddresses)
+}
+
+main()
